59a1806e2ce2c14d712de372e6da7210d147b1bb
Tristan Emmerson <tristan@stickerslug.com>
2020-08-21 10:10:22 -0700

    create Pet model

---
+++ AnimalShelter/Models/Pet.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int PetId { get; set; }
+    public string PetName { get; set; }
+    public string PetAdmissionDate { get; set; }
+    public string PetTaxonomy { get; set; }
+    public string PetBreed { get; set; }
+    public string PetDescription { get; set; }
+
+  }
+}
