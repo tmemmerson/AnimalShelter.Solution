9fb933b82830a570abb069ba4a1fa6d1e84b08c8
Tristan Emmerson <tristan@stickerslug.com>
2020-08-21 11:22:59 -0700

    update Startup.cs file

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using AnimalShelter.Models;
 
 namespace AnimalShelter
 {
@@ -25,6 +27,8 @@ namespace AnimalShelter
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AnimalShelterContext>(opt =>
+                opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -41,7 +45,7 @@ namespace AnimalShelter
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+           /*  app.UseHttpsRedirection(); */
             app.UseMvc();
         }
     }
