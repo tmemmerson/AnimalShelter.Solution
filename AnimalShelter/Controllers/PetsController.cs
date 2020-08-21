e6ff60d82edbeedb91f1b81bf2b4b1b518849908
Tristan Emmerson <tristan@stickerslug.com>
2020-08-21 16:17:53 -0700

    refactor and clean up code structure

--- AnimalShelter/Controllers/PetsController.cs
+++ AnimalShelter/Controllers/PetsController.cs
@@ -18,13 +18,13 @@ namespace AnimalShelter.Controllers
         {
             _db = db;
         }
-        
+
     
-   // GET api/pets
+   //GET api/pets
     [HttpGet]
     public ActionResult<IEnumerable<Pet>> Get(string petName, string petAdmissionDate, string petTaxonomy, string petBreed, string petDescription)
     {
-        var query = _db.Pets.AsQueryable(); //standard
+        var query = _db.Pets.AsQueryable();
 
         if (petName != null)
         {
@@ -55,47 +55,47 @@ namespace AnimalShelter.Controllers
     }
 
 
-        [HttpPost]
-        public void Post([FromBody] Pet pet)
-        {
-            _db.Pets.Add(pet);
-            _db.SaveChanges();
-        }
+    [HttpPost]
+    public void Post([FromBody] Pet pet)
+    {
+        _db.Pets.Add(pet);
+        _db.SaveChanges();
+    }
 
-        [HttpGet ("{id}")]
-        public ActionResult<Pet> Get(int id)
-        {
-        return _db.Pets.FirstOrDefault(entry => entry.PetId == id);
-        }
-        
-        //Pagination
-        [HttpGet("page")]
-        public ActionResult GetPage([FromQuery] UrlQuery urlQuery)
-        {
-            var validUrlQuery = new UrlQuery(urlQuery.PageNumber, urlQuery.PageSize);
-            var pagedData = _db.Pets
-                .OrderBy(pet => pet.PetId)
-                .Skip((validUrlQuery.PageNumber - 1) * validUrlQuery.PageSize)
-                .Take(validUrlQuery.PageSize);
-            return Ok(pagedData);
-        }
+    [HttpGet ("{id}")]
+    public ActionResult<Pet> Get(int id)
+    {
+    return _db.Pets.FirstOrDefault(entry => entry.PetId == id);
+    }
+    
+    //pagination
+    [HttpGet("page")]
+    public ActionResult GetPage([FromQuery] UrlQuery urlQuery)
+    {
+        var validUrlQuery = new UrlQuery(urlQuery.PageNumber, urlQuery.PageSize);
+        var pagedData = _db.Pets
+            .OrderBy(pet => pet.PetId)
+            .Skip((validUrlQuery.PageNumber - 1) * validUrlQuery.PageSize)
+            .Take(validUrlQuery.PageSize);
+        return Ok(pagedData);
+    }
 
-        // PUT api/pets/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Pet pet)
-        {
-            pet.PetId = id;
-            _db.Entry(pet).State = EntityState.Modified;
-            _db.SaveChanges();
-        }
+    //PUT api/pets/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Pet pet)
+    {
+        pet.PetId = id;
+        _db.Entry(pet).State = EntityState.Modified;
+        _db.SaveChanges();
+    }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-            var petToDelete = _db.Pets.FirstOrDefault(entry => entry.PetId == id);
-            _db.Pets.Remove(petToDelete);
-            _db.SaveChanges();
-        }
+    //DELETE api/pets/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+        var petToDelete = _db.Pets.FirstOrDefault(entry => entry.PetId == id);
+        _db.Pets.Remove(petToDelete);
+        _db.SaveChanges();
+    }
     }
 }
